b0b339f2636fc364cbfce8af7ef56a1ff9a882cf
Elliott Spira <elliottspira@gmail.com>
2015-06-18 12:03:35 +1000

    exposing Charts and exporting on client

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'em0ney:chartjs',
-  version: '0.0.1',
+  version: '1.0.2',
   summary: 'ChartJS - awesome charts packaged for meteor',
   git: 'https://github.com/em0ney/meteor-chartjs.git',
   documentation: 'README.md'
@@ -8,5 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('chartjs.js');
+  api.addFiles('chart.js', 'client');
+
+  api.export('Chart', 'client');
 });
